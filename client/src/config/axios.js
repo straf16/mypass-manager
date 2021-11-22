c89c050abe4e899f18e287f461c8e419bfa92f88
fadhilah1611 <fadhilah@cumi.id>
2021-11-23 02:22:48 +0700

    update backend url to use production url

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,9 +1,7 @@
 import axios from 'axios'
 
-// backend server url inactive due to aws account suspended, use local instead
 const instance = axios.create({
-  // baseURL: 'http://pass-manager-server.ahmadfd.site'
-  baseURL: `http://localhost:3010`
+  baseURL: 'https://mypassword-manager.herokuapp.com'
 })
 
 export default instance
